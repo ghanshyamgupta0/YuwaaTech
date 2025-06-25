ea39570efed9ed7a2e89a144335f28c00c185cfa
ghanshyamgupta0 <guptahero056@gmail.com>
2025-06-25 22:39:46 +0545

    Change SESSION_DRIVER to file to fix session error

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/index', [PageController::class, 'index'])->name('index');
+Route::get('/', [PageController::class, 'index'])->name('index');
